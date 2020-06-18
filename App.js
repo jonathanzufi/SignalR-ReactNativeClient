72ba058eaea1ea2d76e61d3416716d0e7b0deeff
Jonathan Zufi <jonathanzufi@gmail.com>
2020-06-18 11:01:41 -0400

    Added some more comments and formatting

--- App.js
+++ App.js
@@ -47,6 +47,9 @@ const App: () => React$Node = () => {
   // Initialize the hub endpoint and set up routes for incoming events
   useEffect(() => {
 
+    // Setting the log level to Debug will generate tons more diagnostic
+    // messages in the console log which can help give a deeper understanding
+    // of how SignalR works and what it's doing under the covers
     const connection = new HubConnectionBuilder()
       .withUrl(hub_endpoint)
       .configureLogging(LogLevel.Debug)
@@ -75,6 +78,9 @@ const App: () => React$Node = () => {
       })]);
     });
 
+    // This isn't very elegant but I'm still learning about scope and state in React Native
+    // This seemed like the logical way to make the connection object available to the 'Reconnect' button
+    // but I think the connection object/handler should be encapsulated into it's own component
     setConn(connection);
 
   }, []);
@@ -89,7 +95,7 @@ const App: () => React$Node = () => {
             <Text style={styles.sectionTitle}>SignalR Demo</Text>
             <Text style={styles.connectedto}>{connectionState}</Text>
 
-            <TextInput placeholder='Username' style={styles.fields}
+            <TextInput placeholder='User' style={styles.fields}
               onChangeText={text => onChangeUserText(text)} value={user}
             />
 
@@ -102,9 +108,9 @@ const App: () => React$Node = () => {
               <TouchableOpacity style={styles.button} onPress={() => {
                 if (user.length === 0 || message.length === 0)
                   return;
-                console.log(`Sending ${user}`);
+                console.log(`Sending ${message} from ${user}`);
                 conn.invoke("SendMessage", `${user}`, `${message}`).catch(function (err) {
-                  console.log('Error sending')
+                  console.log(`Error sending ${message} from ${user}: ${err.message}`);
                 });
                 onChangeMessageText('');
               }}>
@@ -153,49 +159,29 @@ const App: () => React$Node = () => {
 
 const styles = StyleSheet.create({
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-    backgroundColor: 'white'
+    marginTop: 32, paddingHorizontal: 24, backgroundColor: 'white'
   },
   connectedto: {
-    fontSize: 10,
-    fontStyle: 'italic'
+    fontSize: 10, fontStyle: 'italic'
   },
   fields: {
-    marginTop: 20,
-    paddingLeft: 10,
-    fontSize: 15,
-    height: 40,
-    borderWidth: 1
+    marginTop: 20, paddingLeft: 10, fontSize: 15, height: 40, borderWidth: 1
   },
   sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: 'black',
+    fontSize: 24, fontWeight: '600', color: 'black',
   },
   button: {
-    borderWidth: 2,
-    borderColor: "#89AAFF",
-    width: 109,
-    height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 20,
-    marginRight: 20
+    borderWidth: 2, borderColor: "#89AAFF", width: 109, height: 50, alignItems: 'center',
+    justifyContent: 'center', marginTop: 20, marginRight: 20
   },
   buttonText_enabled: {
-    fontSize: 20,
-    color: 'black'
+    fontSize: 20, color: 'black'
   },
   buttonText_disabled: {
-    fontSize: 20,
-    color: '#dfe1e6'
+    fontSize: 20, color: '#dfe1e6'
   },
   messagelogitem: {
-    backgroundColor: '#f9c2ff',
-    padding: 10,
-    marginVertical: 4,
-    marginHorizontal: 4,
+    backgroundColor: '#f9c2ff', padding: 10, marginVertical: 4, marginHorizontal: 4,
   },
   messageList: {
     marginTop: 90
